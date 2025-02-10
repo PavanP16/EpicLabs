12b13679b38713fee93b5451a6871640d0d98df2
Pavan Kumar <pavankumar.padarthy@ukg.com>
2025-02-10 14:24:14 +0530

    Final commit

---
+++ EmployeeAttendaceTracker/src/AttendanceTracker.java
@@ -0,0 +1,154 @@
+import java.util.*;
+
+class Employee{
+
+    private int employeeId;
+    private String employeeName;
+    private String attendanceStatus;
+    static ArrayList<String> AttendanceList = new ArrayList<>();
+
+    public Employee(){
+
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setAttendanceStatus(String attendanceStatus) {
+        this.attendanceStatus = attendanceStatus;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getAttendanceStatus() {
+        return attendanceStatus;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public Employee(int employeeId, String employeeName, String attendanceStatus) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.attendanceStatus = attendanceStatus;
+        AttendanceList.add(employeeId + "," +employeeName + "," + attendanceStatus);
+    }
+
+    static public void printEmployees(){
+
+        if(AttendanceList.isEmpty()) {
+            System.out.println("No Employees.... .");
+            return ;
+        }
+
+        for (String it : AttendanceList){
+            System.out.println(it);
+        }
+    }
+
+    static public String generateAttendanceReport(ArrayList<String> attendanceList){
+
+        int presentCount = 0;
+        int absentCount = 0;
+        for (String status : attendanceList) {
+            if (status.contains("Present")) {
+                presentCount++;
+            } if (status.contains("Absent")) {
+                absentCount++;
+            }
+        }
+        StringBuilder report = new StringBuilder();
+
+        report.append("Attendance Report :- \n");
+        report.append("Total Employees: ").append(attendanceList.size()).append("\n");
+        report.append("Employees Present: ").append(presentCount).append("\n");
+        report.append("Employees Absent: ").append(absentCount);
+
+        return report.toString();
+
+    }
+
+    public static String intToString(int id) {
+        return String.valueOf(id);
+    }
+
+    public static int stringToInt(String id) {
+        return Integer.parseInt(id);
+    }
+
+    static public void line(){
+        System.out.println("-------------------------------------------------");
+    }
+}
+
+class AttendanceTracker{
+    public static void main(String[] args) {
+
+        System.out.println("*********** Attendance Tracking System for UKG ***********");
+
+        int val = 0;
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            Employee.line();
+            System.out.println("1.Add Employees");
+            System.out.println("2.Print the Employees");
+            System.out.println("3.Generate Attendance Report");
+            System.out.println("4.Exit");
+            System.out.print("Enter your choice :- ");
+            val = sc.nextInt();
+            Employee.line();
+
+            switch (val){
+
+                case 1:
+                    System.out.println("Choice 1 :- ");
+                    System.out.print("Enter the number of Employees you want to add :- ");
+                    int n = sc.nextInt();
+                    for (int i = 0; i < n; i++) {
+                        System.out.print("Enter the Employee " + (i + 1)+ " id :- ");
+                        int id = sc.nextInt();
+                        sc.nextLine();
+                        System.out.print("Enter the Employee " + (i + 1)+ " name :- ");
+                        String name = sc.nextLine();
+                        System.out.print("Enter the Employee " + (i + 1)+ " status (Present / Absent) :- ");
+                        String status = sc.nextLine();
+                        new Employee(id,name,status);
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Choice 2 :- ");
+                    Employee.printEmployees();
+                    break;
+
+                case 3:
+                    System.out.println("Choice 3 :- ");
+                    System.out.println(Employee.generateAttendanceReport(Employee.AttendanceList));
+                    break;
+
+                case 4:
+                    System.out.println("Exit...");
+                    break;
+
+                default:
+                    System.out.println("Enter a valid option listed from the list above...");
+                    break;
+
+            }
+
+        }while (val != 4);
+
+
+        sc.close();
+
+    }
+}  
---
+++ WorkHourCalculator/src/WorkHourCalculator.java
@@ -0,0 +1,174 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Employee{
+
+    private int employeeId;
+    private String employeeName;
+    private Double hourlyRate;
+    private ArrayList<Double> workedHours= new ArrayList<>();
+    private static HashMap<Integer,Employee> EmployeesList = new HashMap<>();
+
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public static HashMap<Integer, Employee> getEmployeesList() {
+        return EmployeesList;
+    }
+
+    public ArrayList<Double> getWorkedHours() {
+        return workedHours;
+    }
+
+    public Double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setWorkedHours(ArrayList<Double> workedHours) {
+        this.workedHours = workedHours;
+    }
+
+    public void setHourlyRate(Double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public static void setEmployeesList(HashMap<Integer, Employee> attendanceList) {
+        EmployeesList = attendanceList;
+    }
+
+    public Employee(int id, String name, Double hourlyRate, ArrayList<Double> workedHours) {
+        this.employeeId = id;
+        this.employeeName = name;
+        this.hourlyRate = hourlyRate;
+        this.workedHours = workedHours;
+        EmployeesList.put(id,this);
+    }
+
+    public Double calculateTotalHours(ArrayList<Double> workedHours) {
+        double totalHours = 0;
+        for (double hours : workedHours){
+            totalHours += hours;
+        }
+        return totalHours;
+    }
+
+    public Double calculateWeeklyPay(Double totalHours, Double hourlyRate) {
+        return totalHours * hourlyRate;
+    }
+
+
+    static public void line(){
+        System.out.println("-------------------------------------------------");
+    }
+
+}
+
+public class WorkHourCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("*********** Work Hour Calculator Tracking System for UKG ***********");
+
+        int val = 0;
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            Employee.line();
+            System.out.println("1.Add Employees");
+            System.out.println("2.Check your 'Total Hours' in the week");
+            System.out.println("2.Check your 'Weekly Pay' in the week");
+            System.out.println("3.Generate Attendance Report");
+            System.out.println("4.Exit");
+            System.out.print("Enter your choice :- ");
+            val = sc.nextInt();
+            Employee.line();
+
+            switch (val){
+
+                case 1:
+                    System.out.println("Choice 1 :- ");
+                    System.out.print("Enter the number of Employees you want to add :- ");
+                    int n = sc.nextInt();
+                    for (int i = 0; i < n; i++) {
+                        System.out.print("Enter the Employee " + (i + 1)+ " id :- ");
+                        int id = sc.nextInt();
+                        sc.nextLine();
+                        System.out.print("Enter the Employee " + (i + 1)+ " name :- ");
+                        String name = sc.nextLine();
+                        System.out.print("Enter the Employee " + (i + 1)+ " Hourly rate :- ");
+                        Double hourlyRate = sc.nextDouble();
+                        System.out.println("Enter the worked hours for the week : ");
+                        ArrayList<Double> workedHours = new ArrayList<>();
+                        for (int j = 0 ; j < 7 ; j++){
+                            System.out.print("Enter the worked hours for day " + (j + 1) + " :- ");
+                            double hours = sc.nextDouble();
+                            workedHours.add(hours);
+                        }
+
+                        new Employee(id,name,hourlyRate,workedHours);
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Choice 2 :- ");
+                    System.out.print("Enter your id to check the total hours :- ");
+                    int id = sc.nextInt();
+                    Employee emp = Employee.getEmployeesList().get(id);
+                    if(emp == null){
+                        System.out.println("Employee not found....");
+                        break;
+                    }
+                    System.out.println("Total number of worked hours in the week are " + emp.calculateTotalHours(emp.getWorkedHours()));
+
+                    break;
+
+                case 3:
+                    System.out.println("Choice 3 :- ");
+                    System.out.print("Enter your id to check your weekly pay :- ");
+                    int e_id = sc.nextInt();
+                    Employee e = Employee.getEmployeesList().get(e_id);
+                    if(e == null){
+                        System.out.println("Employee not found....");
+                        break;
+                    }
+                    double totalHours = e.calculateTotalHours(e.getWorkedHours());
+                    System.out.println("Your weekly pay is " + e.calculateWeeklyPay(totalHours,e.getHourlyRate()));
+                    break;
+
+
+                case 4:
+                    System.out.println("Choice 4 :- ");
+                    System.out.print("Enter your id to check your weekly pay :- ");
+                    int emId = sc.nextInt();
+                    Employee em = Employee.getEmployeesList().get(emId);
+                    if(em == null){
+                        System.out.println("Employee not found....");
+                        break;
+                    }
+                    double totalHoursEm = em.calculateTotalHours(em.getWorkedHours());
+                    System.out.println("Your weekly pay is " + em.calculateWeeklyPay(totalHoursEm,em.getHourlyRate()));
+                    break;
+
+                default:
+                    System.out.println("Enter a valid option listed above...");
+                    break;
+
+            }
+
+        }while (val != 4);
+    }
+}
